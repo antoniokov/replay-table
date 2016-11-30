b3a2a85dddd1d31ffc4a4d021c4a58876265dc96
Anton Iokov <it.iokov@gmail.com>
2016-11-30 14:44:38 +0300

    add F1 and ЧГК presets

--- src/config.js
+++ src/config.js
@@ -1,59 +1,65 @@
 export default {
     "default": {
-
+        //input file format. For now only changesTable is supported. See an example: https://s3-us-west-2.amazonaws.com/replay-table/csv/football/england/premier-league/2015-2016.csv
+        //String
         inputType: 'changesTable',
 
-       //resultName is used for color coding. There are three options: 'victory', 'draw' or 'defeat'
-       //default = 3: 'victory', 1: 'draw', 0: 'defeat'
-       //Object: key — result, value — name
-       resultName: {
-            3: 'victory',
-            1: 'draw',
-            0: 'defeat'
-        },
+        //'Position', 'Rank',...
+        //String
+        positionName: '#',
 
-        //focus on particular items (teams, players)
-        //default = empty Array
+        //'Team', 'Player', 'Driver',... When set to undefined gets name from data source if possible
+        //String
+        itemName: undefined,
+
+        //focus on particular items (teams, players, drivers). ['Liverpool', 'Everton'], for example
         //Array of Strings
         focusedItems: [],
 
+        //'Points', 'Wins',...
+        //String
+        totalName: 'Points',
+
+        //['Australia', 'Bahrain',...] for F1, for example. When set to undefined gets names from data source if possible; if not uses round number
+        //Array of Strings
+        roundsNames: undefined,
+
+        //number of round to start from. When set to undefined shows the last round
+        //Number
+        startFromRound: undefined,
+
         //animation duration in ms
-        //default = 1800
         //Number
         animationDuration: 1800,
 
         //determines position when totals are equal. Can be 'no ties' (1, 2, 3, 4,...), 'highest' (1, 2, 2, 4,...) and 'range' (1, 2-3, 2-3, 4,...)
-        //default = 'no ties'
         //String
         tiesResolution: 'no ties',
 
-        //number of round to start from
-        //default = undefined (show the last round)
-        //Number
-        startFromRound: undefined,
-
-        //'Position', 'Rank',...
-        //default = '#'
-        //String
-        positionName: '#',
-
-        //'Team', 'Player', 'Driver',...
-        //default = undefined (get from data source if possible)
-        //String
-        itemName: undefined,
-
-        //'Points', 'Wins',...
-        //default: 'Points'
-        //String
-        totalName: 'Points',
-
-        //['Australia', 'Bahrain',...] for F1
-        //default = undefined (get from data source if possible, if not use round number)
-        //Array of Strings
-        roundsNames: undefined
+        //resultName is used for color coding and animation. There are three options out of the box: 'victory', 'draw' or 'defeat'
+        //Object: key — result, value — name
+        resultName: {
+            3: 'victory',
+            1: 'draw',
+            0: 'defeat'
+        }
     },
 
-    "football": {
-        totalName: 'Pts'
+    "F1": {
+        itemName: 'Driver',
+        resultName: {
+            25: 'gold',
+            18: 'silver',
+            15: 'bronze'
+        }
+    },
+
+    "ЧГК": {
+        itemName: 'Команда',
+        totalName: 'Взято',
+        tiesResolution: 'range',
+        resultName: {
+            1: 'victory'
+        }
     }
 }
