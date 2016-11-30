14f786cdbf3b0138ed7984d0e6ef8fee5709d98c
Anton Iokov <it.iokov@gmail.com>
2016-11-30 15:37:58 +0300

    lowercase directories

--- src/Auxiliary/stableSort.js
+++
@@ -1,5 +0,0 @@
-export default function (array, compareFunction) {
-    return array.map((o,i) => ({obj: o, idx: i}))
-        .sort((a,b) => compareFunction(a.obj, b.obj) ? compareFunction(a.obj, b.obj) : a.idx - b.idx)
-        .map(item => item.obj);
-}
--- src/Auxiliary/transpose.js
+++
@@ -1,4 +0,0 @@
-export default function(matrix) {
-    return Object.keys(matrix[0])
-        .map(colNumber => matrix.map(rowNumber => rowNumber[colNumber]));
-}
