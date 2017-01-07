ab89e4ff3f0da751ba310c7a5690afdd8e06c870
Anton Iokov <it.iokov@gmail.com>
2017-01-07 22:46:13 +0300

    add SeasonTable component

--- src/app/TableContainer.js
+++ src/app/TableContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import FlipMove from 'react-flip-move';
-import getPrintableNumber from '../auxiliary/getPrintableNumber';
 import ControlPanel from './components/ControlPanel';
+import SeasonTable from './components/SeasonTable';
 import './TableContainer.css';
 
 
@@ -12,7 +11,6 @@ class TableContainer extends Component {
         this.state = Object.assign({
             currentRound: this.props.startFromRound,
             previousRound: null,
-            areRoundsConsecutive: true,
             isPlaying: false,
             focusedItems: this.props.focusedItems ? new Set([...this.props.focusedItems]) : new Set(),
             mode: 'season'
@@ -20,17 +18,14 @@ class TableContainer extends Component {
     }
 
     getChanges (previousRound, currentRound) {
-        return [...this.props.results[currentRound].entries()].reduce((currentStats, [item, result]) => {
-            const change = previousRound === null
-                ? result.change
-                : result.total - this.props.results[previousRound].get(item).total;
+        const changes = new Map([...this.props.results[currentRound].entries()].map(([item, result]) => {
+            return [item, previousRound === null ? result.change : result.total - this.props.results[previousRound].get(item).total]
+        }));
 
-            return {
-                maxAbsChange: Math.max(Math.abs(change), currentStats.maxAbsChange),
-                maxAbsResultChange: Math.max(Math.abs(result.change), currentStats.maxAbsResultChange),
-                allChangesMapped: currentStats.allChangesMapped && (!!result.result || result.change === null)
-            }
-        }, { maxAbsChange: 0, maxAbsResultChange: 0, allChangesMapped: true });
+        return {
+            changes: changes,
+            maxAbsChange: Math.max(...[...changes.entries()].map(([item, change]) => Math.abs(change)))
+        }
     }
 
     goToRound (roundNumber) {
@@ -39,7 +34,6 @@ class TableContainer extends Component {
             return new Promise(resolve => this.setState(Object.assign({
                 previousRound: this.state.currentRound,
                 currentRound: roundNumber,
-                areRoundsConsecutive: Math.abs(this.state.currentRound - roundNumber) === 1,
                 isMoving: true
             }, changes), () => {
                 return setTimeout(() => this.setState({ isMoving: false }, resolve), this.props.animationDuration);
@@ -76,8 +70,7 @@ class TableContainer extends Component {
         }
     }
 
-
-    highlightRow (item) {
+    switchFocus (item) {
         const newFocusedItems = this.state.focusedItems;
         if (newFocusedItems.has(item)) {
             newFocusedItems.delete(item)
@@ -87,70 +80,32 @@ class TableContainer extends Component {
         this.setState({ focusedItems: newFocusedItems });
     }
 
-    getRowStyle (result, change) {
-        const styleObject = {};
-        const resultClass = result.result;
+    renderTable () {
+        return (
+            <SeasonTable
+                positionName={this.props.positionName}
+                itemName={this.props.itemName}
+                totalName={this.props.totalName}
 
-        const customStyleNeeded = this.state.mode === 'round' && !this.state.allChangesMapped;
-        const animationNeeded = this.state.isMoving && this.state.currentRound > 0;
-        const customAnimationNeeded = animationNeeded && (!this.state.areRoundsConsecutive || !this.state.allChangesMapped);
+                calculatedColumns={this.props.calculatedColumns}
+                extraColumnsNames={this.props.extraColumnsNames}
 
-        if (!animationNeeded) {
-            if (customStyleNeeded) {
-                const color = result.change >= 0 ? '94,179,26' : '179,82,82';
-                const changeIntensity = result.change
-                    ? Math.max(Math.round(10*Math.abs(result.change)/this.state.maxAbsResultChange)/10, 0.1)
-                    : 0;
-                styleObject.backgroundColor = `rgba(${color},${changeIntensity})`;
-            }
-        } else {
-            if (customAnimationNeeded) {
-                const color = change >= 0 ? 'green' : 'red';
-                const changeIntensity = change
-                    ? Math.max(10*Math.round(10*Math.abs(change)/this.state.maxAbsChange), 0.1)
-                    : 0;
-                styleObject.animation = `${color}-${changeIntensity} ${this.props.animationDuration}ms`
-            } else {
-                styleObject.animation = `${resultClass} ${this.props.animationDuration}ms`;
-            }
-        }
+                results={this.props.results[this.state.currentRound]}
+                changes={this.state.changes}
+                maxAbsChange={this.state.maxAbsChange}
+                areRoundsConsecutive={this.state.previousRound === null || Math.abs(this.state.currentRound - this.state.previousRound) === 1}
 
-        return styleObject;
+                mode={this.state.mode}
+                isMoving={this.state.isMoving}
+
+                switchFocus={this.switchFocus}
+                isFocused={item => this.state.focusedItems.size === 0 || this.state.focusedItems.has(item)}
+                animationDuration={this.props.animationDuration}
+                showChangeDuringAnimation={this.props.showChangeDuringAnimation} />
+        );
     }
 
-    getRowClasses (item, result) {
-        const classes = ['row'];
-        if (this.state.mode === 'round' && result.result) {
-            classes.push(result.result);
-        }
-
-        if (this.state.focusedItems.size === 0 || this.state.focusedItems.has(item)) {
-            classes.push('focus')
-        }
-
-        return classes.join(' ');
-    }
-
-    getTotalText (result, change) {
-        if (this.state.currentRound === 0) {
-            return result.total;
-        }
-
-        const shouldAnimateChange =  this.state.isMoving && (this.props.showChangeDuringAnimation || !this.state.areRoundsConsecutive);
-        if (shouldAnimateChange) {
-            return getPrintableNumber(change, true);
-        } else {
-            switch (this.state.mode) {
-                case 'round':
-                    return getPrintableNumber(result.change, true);
-                case 'season':
-                    return getPrintableNumber(result.total);
-            }
-        }
-    }
-
-
-    render() {
+    render () {
         return (
             <div className="replay-table-wrap">
                 <ControlPanel
@@ -170,50 +125,7 @@ class TableContainer extends Component {
                     tableName={this.props.tableName}
                     showProgressBar={this.props.showProgressBar} />
 
-                <table className="r-table">
-                    <thead>
-                    <tr>
-                        <th className="position">{this.props.positionName}</th>
-                        <th className="item">{this.props.itemName}</th>
-                        {Object.keys(this.props['calculatedColumns']).map(key => {
-                            const name = this.props['calculatedColumns'][key];
-                            return <th key={name} className="calculated">{name}</th>;
-                        })}
-                        {this.props['extraColumnsNames'].map(name => <th className="extra" key={name}>{name}</th>)}
-                        <th className="total">{this.props.totalName}</th>
-                    </tr>
-                    </thead>
-                    <FlipMove
-                        delay={this.props.animationDuration/2}
-                        duration={this.props.animationDuration/2}
-                        typeName='tbody' >
-
-                        {[...this.props.results[this.state.currentRound].entries()]
-                            .map(([item, result]) => {
-                                const change = this.state.areRoundsConsecutive
-                                    ? result.change
-                                    : result.total - this.props.results[this.state.previousRound].get(item).total;
-
-                                return (
-                                    <tr key={item}
-                                        style={this.getRowStyle.bind(this)(result, change)}
-                                        className={this.getRowClasses.bind(this)(item, result)}
-                                        onClick={() => this.highlightRow(item)}>
-
-                                        <td className="position">{result.position}</td>
-                                        <td className="item">{item}</td>
-                                        {Object.keys(this.props['calculatedColumns']).map(key =>
-                                            <td key={key} className="calculated">{result[key]}</td>
-                                        )}
-                                        {this.props.extraColumnsNames.map(name =>
-                                            <td key={name} className="extras">{result.extras[name]}</td>
-                                        )}
-                                        <td className="total">{this.getTotalText.bind(this)(result, change)}</td>
-                                    </tr>
-                                );
-                            })}
-                    </FlipMove>
-                </table>
+                {this.renderTable()}
             </div>
         );
     }
---
+++ src/app/components/SeasonTable.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import FlipMove from 'react-flip-move';
+import getPrintableNumber from '../../auxiliary/getPrintableNumber';
+
+
+function getRowStyle (isMoving, styleParams, result, change, maxAbsChange, roundChange, maxAbsRoundChange) {
+    const styleObject = {};
+
+    if (!isMoving) {
+        if (styleParams.customStyleNeeded) {
+            const color = roundChange >= 0 ? '94,179,26' : '179,82,82';
+            const changeIntensity = roundChange
+                ? Math.max(Math.round(10*Math.abs(roundChange)/maxAbsRoundChange)/10, 0.1)
+                : 0;
+            styleObject.backgroundColor = `rgba(${color},${changeIntensity})`;
+        }
+    } else {
+        if (styleParams.customAnimationNeeded) {
+            const color = change >= 0 ? 'green' : 'red';
+            const changeIntensity = change
+                ? Math.max(10*Math.round(10*Math.abs(change)/maxAbsChange), 0.1)
+                : 0;
+            styleObject.animation = `${color}-${changeIntensity} ${styleParams.animationDuration}ms`
+        } else {
+            styleObject.animation = `${result} ${styleParams.animationDuration}ms`;
+        }
+    }
+
+    return styleObject;
+}
+
+function getTotalText (currentRound, mode, shouldAnimateChange, change, roundChange, total) {
+    if (currentRound === 0) {
+        return getPrintableNumber(total);
+    }
+
+    if (shouldAnimateChange) {
+        return getPrintableNumber(change, true);
+    } else {
+        switch (mode) {
+            case 'round':
+                return getPrintableNumber(roundChange, true);
+            case 'season':
+                return getPrintableNumber(total);
+        }
+    }
+}
+
+function SeasonTable (props) {
+    const allResultsMapped = [...props.results.entries()].every(([item, result]) => !!result.result);
+    const maxAbsRoundChange = Math.max(...[...props.results.entries()].map(([item, result]) => Math.abs(result.change)));
+
+    const styleParams = {
+        customStyleNeeded: props.mode === 'round' && !allResultsMapped,
+        customAnimationNeeded: props.isMoving && (!props.areRoundsConsecutive || !allResultsMapped),
+        shouldAnimateChange:  props.isMoving && (props.showChangeDuringAnimation || props.areRoundsConsecutive),
+        animationDuration: props.animationDuration
+    };
+
+    return (
+        <table className="r-table">
+            <thead>
+            <tr>
+                <th className="position">{props.positionName}</th>
+                <th className="item">{props.itemName}</th>
+                {Object.keys(props.calculatedColumns).map(key => {
+                    return <th key={props.calculatedColumns[key]} className="calculated">{props.calculatedColumns[key]}</th>;
+                })}
+                {props.extraColumnsNames.map(name => {
+                    return <th className="extra" key={name}>{name}</th>;
+                })}
+                <th className="total">{props.totalName}</th>
+            </tr>
+            </thead>
+            <FlipMove
+                delay={props.animationDuration/2}
+                duration={props.animationDuration/2}
+                typeName='tbody' >
+
+                {[...props.results.entries()]
+                    .map(([item, result]) => {
+                        const classCandidates = [
+                            { condition: props.mode === 'round', class: result.result || '' },
+                            { condition: props.isFocused(item), class: 'focus'}
+                        ];
+                        const rowClasses = classCandidates
+                            .filter(element => element.condition)
+                            .map(element => element.class)
+                            .join(' ');
+
+                        return (
+                            <tr key={item}
+                                style={getRowStyle(props.isMoving, styleParams, result.result, props.changes.get(item),
+                                    props.maxAbsChange, result.change, maxAbsRoundChange) }
+                                className={rowClasses}
+                                onClick={() => props.switchFocus(item)}>
+
+                                <td className="position">{result.position}</td>
+                                <td className="item">{item}</td>
+                                {Object.keys(props.calculatedColumns).map(key =>
+                                    <td key={key} className="calculated">{result[key]}</td>
+                                )}
+                                {props.extraColumnsNames.map(name =>
+                                    <td key={name} className="extras">{result.extras[name]}</td>
+                                )}
+                                <td className="total">{getTotalText(props.currentRound, props.mode,
+                                    styleParams.shouldAnimateChange, props.changes.get(item), result.change, result.total)}</td>
+                            </tr>
+                        );
+                    })}
+            </FlipMove>
+        </table>
+    );
+}
+
+export default SeasonTable;
